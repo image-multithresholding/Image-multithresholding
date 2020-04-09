e7c256df779e900319e7ef627d1e8771012c2518
arey1911 <63259737+arey1911@users.noreply.github.com>
2020-04-09 16:38:41 -0300

    Add files via upload

---
+++ RCodes/Functions/clusterVar.R
@@ -0,0 +1,44 @@
+############################################################################################
+
+## Compute the variance of a given cluster 
+
+############################################################################################
+
+# Arguments:
+# prob the probability vector of gray levels 0,1,...,L
+# clust a clust of the gray levels
+
+# Value:
+# clusterVar returns an object with class "numeric" 
+
+############################################################################################
+
+clusterVar <- function(prob, clust){
+  
+  # Compute the cluster probability
+  
+  clusterProb <- sum(prob[clust+1])
+  
+  # Compute the cluster mean
+  
+  mu <- clusterMean(prob, clust)
+  
+  # Initialize
+  
+  term <- vector()
+  
+  # Compute the variance of each element in the cluster
+  
+  for (j in (clust+1)){
+    term[j] <- (j-1-mu)^2*prob[j]
+  }
+  
+  # Compute the variance of the cluster
+  
+  vc <- sum(term, na.rm = T)/clusterProb
+  
+  # Output
+  
+  return(vc)
+}
+
