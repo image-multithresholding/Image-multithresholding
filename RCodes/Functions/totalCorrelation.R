9f6c71ae59663d6e81db60be2d11f48539da3a38
arey1911 <63259737+arey1911@users.noreply.github.com>
2020-04-09 19:50:49 -0300

    Add files via upload

---
+++ RCodes/Functions/totalCorrelation.R
@@ -0,0 +1,63 @@
+############################################################################################
+
+## Compute the total correlation according to a list of breaking gray levels
+
+############################################################################################
+
+# Arguments:
+# prob the probability vector of gray levels 0,1,...,L-1
+# levels a list of gray levels which give the breaks
+
+# Value:
+# totalCorrelation returns a list with class "numeric" 
+
+############################################################################################
+
+
+totalCorrelation <- function(prob, levels){
+  
+  # Find the probabilities according to the given levels
+  
+  p <- probUpToLevel(prob, levels)
+  
+  # Find the number of breaks and probabilities
+  
+  n <- length(levels)
+  m <- length(prob)
+  
+  # Shift levels to start in 1
+  
+  levels <- levels+1
+  
+  # Initialize
+  
+  C <- vector()
+  
+  if (n==1){
+    
+    # Find the correlation of both intervals
+    
+    C[1] <- -log(sum(prob[1:levels]^2)/p[1]^2)
+    C[2] <- -log(sum(prob[(levels+1):m]^2)/p[2]^2)
+   } 
+  
+  else {
+  
+  # Find the correlation of each interval
+  
+  C[1] <- -log(sum(prob[1:levels[1]]^2)/p[1]^2)
+  for (i in 2:n){
+    C[i] <- -log(sum(prob[(levels[i-1]+1):levels[i]]^2)/p[i]^2)
+  }
+  C[n+1] <- log(sum(prob[(levels[n]+1):m]^2)/p[n+1]^2)
+  }
+  
+  # Find the total correlation
+  
+  TC <- sum(C)
+  
+  # Output
+  
+  return(TC)
+}
+
