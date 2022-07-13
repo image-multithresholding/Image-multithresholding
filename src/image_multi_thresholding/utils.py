e68dcb00bd358fe01ed0971fcc47badf126c30c0
Joaquin Leon <leonjoaquin77@gmail.com>
2022-07-13 19:11:11 -0300

    Fix colors code

--- src/image_multi_thresholding/utils.py
+++ src/image_multi_thresholding/utils.py
@@ -1,22 +1,17 @@
 from PIL.Image import Image, new
 import numpy as np
 
-COLORS = [
-    (93, 46, 70),
-    (173, 106, 108),
-    (208, 173, 167),
-    (232, 214, 203)
-]
-
-def get_thresholded_image(image: np.ndarray, thresholds: list) -> Image:
+def get_thresholded_image(image: np.ndarray, thresholds: list, colors: list) -> Image:
+    if len(thresholds) > len(colors) + 1:
+        raise Exception("number of colors doesn't match number of classes")
     img = new("RGB", (image.shape[1], image.shape[0]))
     for x in range(image.shape[1]):
         for y in range(image.shape[0]):
             pixel = image[y][x][0]
             for i, thr in enumerate(thresholds):
                 if pixel < thr:
-                    img.putpixel((x, y), COLORS[i])
+                    img.putpixel((x, y), colors[i])
                     break
             else:
-                img.putpixel((x, y), COLORS[len(thresholds)])
+                img.putpixel((x, y), colors[len(thresholds)])
     return img
